e03434ac3e11c63120c78437fa5645a4fa2711b1
Jim Kennedy <kennedyomata@gmail.com>
2025-09-26 15:44:02 +0300

    refactor: added api context application wide

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,31 @@ import { SelectedFlightsPage } from "./pages/SelectedFlightsPage";
 import Layout from "./pages/Layout";
 import NotFound from "./pages/NotFound";
 import HomePage from "./pages/HomePage";
+import { ApiProvider } from "./context/ApiProvider";
 
 const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<HomePage />} />
-            <Route path="selected-flights" element={<SelectedFlightsPage />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <ApiProvider>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<HomePage />} />
+              <Route
+                path="selected-flights"
+                element={<SelectedFlightsPage />}
+              />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  </ApiProvider>
 );
 
 export default App;
